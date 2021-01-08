241a500577b59f2cae7bfcdaae70456ed6b51862
ryotaroAAA <ryo.ex3991@gmail.com>
2021-01-08 14:57:17 +0900

    ppuちょっと

--- include/ppu.hpp
+++ include/ppu.hpp
@@ -5,17 +5,26 @@
 #include "ram.hpp"
 
 const uint8_t TILE_SIZE = 8;
+const uint8_t V_SPRITE_NUM = 32;
+const uint8_t H_SPRITE_NUM = 30;
 const uint8_t H_SIZE = 240;
 const uint16_t H_SIZE_WITH_VBLANK = 262;
 const uint16_t CYCLE_PER_LINE = 341;
 
 class Ppu {
     private:
+        Casette *cas;
         uint16_t cycle;
         uint16_t line;
+        uint16_t tile_x;
+        uint16_t tile_y;
+        
     public:
         Ppu();
         void run(uint16_t cycle);
+        uint16_t get_block_id();
+        uint16_t get_sprite_id();
+        uint16_t get_parette();
         void build_tile();
         void build_sprite();
         void build_background();
--- src/ppu.cpp
+++ src/ppu.cpp
@@ -21,3 +21,30 @@ void Ppu::run(uint16_t cycle) {
     }
 }
 
+void Ppu:build_tile(uint16_t x, uint16_t y){
+    uint16_t block_id = this->get_block_id(x, y);
+    uint16_t sprite_id = this->get_sprite_id(x, y);
+    uint16_t palette = this->get_palette(x, y);
+    uint16_t palette_id = (palette >> (block_id * 2)) & 0x03;
+    this->build_sprite(sprite_id);
+}
+
+void Ppu:build_sprite(uint8_t sprite_id){
+    for (uint8_t i = 0; x < 16; i++) {
+        for (uint8_t j = 0; x < 8; j++) {
+            uint8_t addr = sprite_id * 16 + i;
+            uint8_t *ram = this->cas->get_char_rom();
+            if (ram & (0x80 >> j)) {
+                sprite[i % 8][j] +=
+                    0x01 << i/8
+            }
+        }
+    }
+}
+
+void Ppu:build_background(){
+    uint8_t y = this->tile_y % H_SPRITE_NUM;
+    for (uint8_t x = 0; x < V_SPRITE_NUM; x++) {
+        uint16_t name_table_id = (x/32);
+    }
+}
